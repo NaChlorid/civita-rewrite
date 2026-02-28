0c438eb90914430a63baaa729839ae20c8129e8c
optimi <optimi@chlorid.org>
2026-02-28 09:48:27 +0100

    Update src/embedium.py

--- src/embedium.py
+++ src/embedium.py
@@ -7,7 +7,7 @@
 from datetime import datetime, UTC
 from disnake import Color, Embed
 from mcstatus import JavaServer
-import config_env as shit_env
+import shit_env
 import random
 
 env = shit_env.Env(".env")
